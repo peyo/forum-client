0867407693b21f877892751488d62ac4299ec43c
peyo <peteryyoon@gmail.com>
2020-12-21 14:27:50 -0800

    Added serverWorker.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,10 @@
-import React, { Component } from 'react';
-import logo from './../logo.svg';
-import './../styles/App.css';
+import React, { Component } from "react";
+import LinkList from "./LinkList";
+
+class App extends Component {
+  render() {
+    return <LinkList />;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
-  InMemoryCache
-} from '@apollo/client';
+  InMemoryCache,
+} from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: "http://localhost:4000",
 });
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 serviceWorker.unregister();
